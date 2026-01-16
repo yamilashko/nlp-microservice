0fbac5dacb1d806571d3d8fca5817e8e4110282a
yamilashko <prodarusya.2005@mail.ru>
2026-01-16 21:47:53 +0300

    tfidf.py

--- server/models/tfidf.py
+++ server/models/tfidf.py
@@ -3,11 +3,7 @@ import numpy as np
 from server.models.bow import bow_matrix
 
 def tfidf_matrix(texts: List[str]) -> Tuple[List[str], np.ndarray]:
-    """
-    TF-IDF (numpy):
-    TF = count(word in doc)
-    IDF = log((N + 1) / (df + 1)) + 1   (сглаженная версия)
-    """
+
     vocabulary, bow = bow_matrix(texts)  # (n_docs, n_words)
     n_docs = bow.shape[0]
 
